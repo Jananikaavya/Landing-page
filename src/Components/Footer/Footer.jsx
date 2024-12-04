4a9def7b64d69e11a45b286d8c8f06f0d62bccf6
janani <jananikaavya1104@gmail.com>
2024-12-04 19:25:13 +0530

    Initial commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>@2024 Edusity. All rights reserved.</p>
+      <ul>
+        <li>Terms of Service</li>
+        <li>Privacy Policy</li>
+      </ul>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/Components/campus/campus-HP.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Campus.css'
+const campus = () => {
+  return (
+    <div className='campus'>
+        <div className="gallery">
+            <img src="" alt=""/>
+        </div>
+
+    </div>
+  )
+}
+
+export default campus
